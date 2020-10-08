39611a27e0defdd552a2b064777655704ec3ed3b
Артём Трофименко <delonartem@yandex.ru>
2020-10-08 11:43:48 +0300

    3.1.2

--- histogram.h
+++ histogram.h
@@ -5,6 +5,15 @@
 
 using namespace std;
 
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+}
+
 void find_minmax (const vector<double>& numbers, double& min, double& max);
+
+vector<size_t> make_histogram(const Input& data);
+
+void show_histogram_text(vector<size_t> bins);
 
 #endif // HISTOGRAM_H_INCLUDED
