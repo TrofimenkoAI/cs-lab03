1e5bb9f8827659fa6820a1bfde97e52009f7d495
Артём Трофименко <delonartem@yandex.ru>
2020-10-08 11:55:04 +0300

    3.2.7

--- main.cpp
+++ main.cpp
@@ -58,6 +58,11 @@ nt main(int argc, char* argv[]) {
             CURLcode res;
             curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
             res = curl_easy_perform(curl);
+            if (res != CURLE_OK)
+            {
+                cout << curl_easy_strerror(res) << endl;
+                exit(1);
+            }
             curl_easy_cleanup(curl);
         }
 
